fa68e53d835e94c0bce76c794271297bbd5532d0
Simon Jentsch <sjentsch@me.com>
2012-07-23 17:56:34 +0200

    OS X Related Resource path system
    when not compiling on OS X or in OS X DEBUG mode the resources are next
    to the executable in folder "data"

---
+++ lazy-crab/ResourcePath.h
@@ -0,0 +1,14 @@
+//
+//  ResourcePath.h
+//  lazy-crab
+//
+//  Created by Simon Jentsch on 23.07.12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef lazy_crab_ResourcePath_h
+#define lazy_crab_ResourcePath_h
+
+
+
+#endif
